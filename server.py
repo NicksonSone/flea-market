0184b49daac7615924cbfdaaf89f54f6546a6694
sone <zhujunqi@jaxus.com>
2015-12-31 11:13:48 +0800

    add Content-Type to allow headers

--- server.py
+++ server.py
@@ -30,7 +30,7 @@ def allow_cross_domain(fun):
         rst = make_response(fun(*args, **kwargs))
         rst.headers['Access-Control-Allow-Origin'] = '*'
         rst.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE,OPTIONS'
-        allow_headers = "Referer,Accept,Origin,User-Agent"
+        allow_headers = "Referer,Accept,Origin,User-Agent, Content-Type"
         rst.headers['Access-Control-Allow-Headers'] = allow_headers
         return rst
     return wrapper_fun
