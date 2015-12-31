836d5f93ba177f143097ee07574a731d132d23da
sone <zhujunqi@jaxus.com>
2015-12-31 10:09:50 +0800

    retry cross domain

--- server.py
+++ server.py
@@ -28,10 +28,10 @@ def allow_cross_domain(fun):
     @wraps(fun)
     def wrapper_fun(*args, **kwargs):
         rst = make_response(fun(*args, **kwargs))
-        rst.headers["Access-Control-Allow-Origin"] = "*"
-        rst.headers["Access-Control-Allow-Methods"] = "PUT, GET, POST, DELETE"
-        allow_headers = "Referer, Accept, Origin, User-Agent"
-        rst.headers["Access-Control-Allow-Headers"] = allow_headers
+        rst.headers['Access-Control-Allow-Origin'] = '*'
+        rst.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+        allow_headers = "Referer,Accept,Origin,User-Agent"
+        rst.headers['Access-Control-Allow-Headers'] = allow_headers
         return rst
     return wrapper_fun
 
