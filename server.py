b33eebb642d640264b0655b284dc8996e12cf45b
sone <zhujunqi@jaxus.com>
2016-01-04 17:48:43 +0800

    auto commit

--- server.py
+++ server.py
@@ -53,7 +53,7 @@ def register():
     # insert data into database
     # userName = request.values.get("userName", "")
     # return jsonify(state=request.values)
-    r = request.form["request"]
+    r = request.form.get("request", "happy")
     return jsonify(state=r)
     s = request.data
     # return jsonify(state=s)
