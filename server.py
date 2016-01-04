4fa7b80dc5d031d295bae205717e8a0f5f09b719
sone <zhujunqi@jaxus.com>
2016-01-04 16:10:53 +0800

    str to dict

--- server.py
+++ server.py
@@ -56,7 +56,7 @@ def register():
     s = request.data
     data = "'" + s[1:-1] + "'"
     r = ast.literal_eval(data)
-    return jsonify(state=r["request"])
+    return jsonify(state=r)
     password = request.form.get("password", "")
     phoneNum = request.form.get("phoneNum", "")
     email = request.form.get("email", "")
