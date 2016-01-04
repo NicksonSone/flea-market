435dac954f11d94395e23c6a89034e02291c249a
sone <zhujunqi@jaxus.com>
2016-01-04 10:57:01 +0800

    test data passing

--- server.py
+++ server.py
@@ -52,7 +52,7 @@ def register():
     # insert data into database
     # userName = request.values.get("userName", "")
     # return jsonify(state=request.values)
-    req = request.form
+    req = request.form.get("userName", "hello")
     req = str(req)
     return jsonify(state=req)
     password = request.form.get("password", "")
