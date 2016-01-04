fc5731c94fb8e6f6de6ebf2c29666e4c61449bb1
sone <zhujunqi@jaxus.com>
2016-01-04 16:41:20 +0800

    auto commit

--- server.py
+++ server.py
@@ -54,8 +54,8 @@ def register():
     # userName = request.values.get("userName", "")
     # return jsonify(state=request.values)
     s = request.data
-    r = ast.literal_eval(s)
-    return jsonify(state=r.__class__.__name__)
+    #r = ast.literal_eval(s)
+    return jsonify(state=s)
     password = request.form.get("password", "")
     phoneNum = request.form.get("phoneNum", "")
     email = request.form.get("email", "")
