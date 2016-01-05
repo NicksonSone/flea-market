d33f3da1caf0980dfb865f686c7d7651624fbc6b
sone <zhujunqi@jaxus.com>
2016-01-05 12:29:21 +0800

    auto commit

--- server.py
+++ server.py
@@ -106,13 +106,14 @@ def register():
     return jsonify(state=1, userId=userId)
 
 
-@app.route("/login", methods=["PUT", "OPTIONS"])
+@app.route("/login", methods=["POST", "OPTIONS"])
 @allow_cross_domain
 def login():
     # find user account
     # check password
 
-    return jsonify(state=request.form["account"])
+    data = parseData()
+    return jsonify(state=data)
     account = data.get("account", "")
     password = data.get("password", "")
 
