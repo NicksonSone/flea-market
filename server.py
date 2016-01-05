5400bdbf616739ae8d7a1fbfce59abc242e8b299
sone <zhujunqi@jaxus.com>
2016-01-05 11:52:44 +0800

    auto commit

--- server.py
+++ server.py
@@ -42,9 +42,8 @@ def allow_cross_domain(fun):
 
 
 def parseData():
-    # data = list(request.form.iterkeys())[0]
+    data = list(request.form.iterkeys())
     # data = ast.literal_eval(data)
-    data = request.form
     return data
 
 
