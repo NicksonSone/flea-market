64a418155303e82bde72d3e74ddeeb88c9be549a
sone <zhujunqi@jaxus.com>
2016-01-09 10:55:10 +0800

    auto commit

--- server.py
+++ server.py
@@ -207,7 +207,7 @@ def edit_user_info():
 @allow_cross_domain
 def create_item():
     userId = int(request.form.get("userId", 0))
-    return jsonify(state=userId)
+    return jsonify(state=userId.__class__.__name__)
     categoryId = int(request.form.get("category", 0))
     subcategoryId = int(request.form.get("subcategory", 0))
     arguable = int(request.form.get("arguable", 0))
