9c0f63b729072706703ca8453535dcda75922bbf
sone <zhujunqi@jaxus.com>
2016-01-10 20:52:36 +0800

    auto commit

--- server.py
+++ server.py
@@ -456,7 +456,7 @@ def get_collected_items():
 @allow_cross_domain
 def test_image_upload():
 
-    return "123"
+    return jsonify(state="11")
     # filed = request.files
     # data = request.get_json()
     # d = request.json
