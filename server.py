230b1c5b5259d6d2ca7cd481dcd06e231307aeb6
sone <zhujunqi@jaxus.com>
2016-01-10 18:43:10 +0800

    auto commit

--- server.py
+++ server.py
@@ -56,7 +56,8 @@ def allow_cross_domain(fun):
         rst.headers['Access-Control-Allow-Origin'] = '*'
         rst.headers['Access-Control-Allow-Methods'] = (
             'PUT,GET,POST,DELETE,OPTIONS')
-        allow_headers = "Referer,Accept,Origin,User-Agent, Content-Type"
+        allow_headers = "Referer,Accept,Origin,User-Agent, Content-Type, \
+            X_FILENAME"
         rst.headers['Access-Control-Allow-Headers'] = allow_headers
         return rst
     return wrapper_fun
