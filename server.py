c3e19ca31c5fcad44729de4ae333659ab0ee230c
sone <zhujunqi@jaxus.com>
2016-01-28 19:20:37 +0800

    auto commit

--- server.py
+++ server.py
@@ -455,7 +455,7 @@ def get_collected_items():
 @app.route("/test", methods=["POST", "OPTIONS"])
 @allow_cross_domain
 def test_image_upload():
-    image = request.files
+    image = request.files.iterkeys()
     return jsonify(image=image)
     bucket = Bucket("avatar")
     bucket.put()
