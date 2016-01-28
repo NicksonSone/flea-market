4de0b9331d1f4be4487397ffd43dc43cd7542977
sone <zhujunqi@jaxus.com>
2016-01-28 22:08:24 +0800

    auto commit

--- server.py
+++ server.py
@@ -458,7 +458,7 @@ def test_image_upload():
     if request.method == 'POST':
         image = request.files['fileList']
         if image:
-            return jsonify(image=image.filename)
+            return jsonify(image=image.mime_type)
     return jsonify(no="2")
     bucket = Bucket("avatar")
     bucket.put()
