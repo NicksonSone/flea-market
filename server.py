36ed84cc6b3cf5647e32d843ce6509d279268d1e
sone <zhujunqi@jaxus.com>
2016-03-05 16:19:32 +0800

    auto commit

--- server.py
+++ server.py
@@ -116,7 +116,7 @@ def front_page():
     cursor.execute(query)
     foreignBooks = cursor.fetchall()
     for book in foreignBooks:
-        return jsonify(name=book[10])
+        return jsonify(name=book[11].__class__.__name__)
         book[11] = datetimeToTimeElement(book[11])
 
     # latest professional material
