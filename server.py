0c2e9c16a7e189b781331e76e4d1655dc747637c
sone <zhujunqi@jaxus.com>
2016-03-09 17:11:22 +0800

    auto commit

--- server.py
+++ server.py
@@ -642,6 +642,7 @@ def image_upload():
             bucket = Bucket("avatar")
             numObejcts = bucket.stat()["objects"]
             imageId = str(numObejcts + 1)
+            return imageId
             bucket.put_object(imageId, image.stream)
             url = bucket.generate_url(imageId)
 
