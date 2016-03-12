75c01d13179e569ec8461637d20a85443d517d6c
sone <zhujunqi@jaxus.com>
2016-03-12 15:58:09 +0800

    auto commit

--- server.py
+++ server.py
@@ -531,7 +531,6 @@ def create_item():
     insert = ("insert into Sell(userId, itemId) values(%s, %s)")
     cursor.execute(insert, (userId, itemId))
     g.db.commit()
-    return jsonify(a=tradeVenue, b=tradeVenue.__class__.__name__)
 
     # create fallsIn relationship between the category and subcategory
     insert = ("insert into FallsIn(itemId, categoryId, subcategoryId) \
