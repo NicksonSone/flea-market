2f5af10a0700caf1eff5934048c41d564c644e2d
sone <zhujunqi@jaxus.com>
2016-03-12 23:35:48 +0800

    auto commit

--- server.py
+++ server.py
@@ -488,6 +488,7 @@ def create_item():
     # decode Chinese character for storage
     title = urllib.unquote(title).decode("utf-8")
     description = urllib.unquote(description).decode("utf-8")
+    return jsonify(a=title, b=description)
 
     #   get sender name
     cursor = g.db.cursor()
@@ -508,7 +509,6 @@ def create_item():
               )
     cursor.execute(insert, params)
     g.db.commit()
-    return "e"
 
     query = ("select last_insert_id() from Item")
     cursor.execute(query)
