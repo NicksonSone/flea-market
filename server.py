c7e35b412d010302fd8100d345d8c0cba8f455f7
sone <zhujunqi@jaxus.com>
2016-03-12 22:45:51 +0800

    auto commit

--- server.py
+++ server.py
@@ -481,8 +481,8 @@ def create_item():
     price = float(request.form.get("price", 0.0))
     tradeVenue = int(request.form.get("tradeVenue", 0))
     title = request.form.get("title", "default")
-    return "h"
     title = urllib.unquote(title).decode("utf-8")
+    return "h"
     description = request.form.get("description", "description")
     picArray = request.form.getlist("picArray[]")
     postDate = datetime.now()
