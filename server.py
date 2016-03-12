883c6251c636e4f341af2bcbd09fc6bb40e937f2
sone <zhujunqi@jaxus.com>
2016-03-12 23:11:10 +0800

    auto commit

--- server.py
+++ server.py
@@ -481,23 +481,12 @@ def create_item():
     price = float(request.form.get("price", 0.0))
     tradeVenue = int(request.form.get("tradeVenue", 0))
     title = str(request.form.get("title", "default"))
-
-
-
-
-
-    title = urllib.unquote(title).decode("utf-8")
-
-    return jsonify(title=title)
-
-
-
-
-
-
-    description = request.form.get("description", "description")
+    description = str(request.form.get("description", "description"))
     picArray = request.form.getlist("picArray[]")
     postDate = datetime.now()
+
+    title = urllib.unquote(title).decode("utf-8")
+    description = urllib.unquote(description).decode("utf-8")
 
     #   get sender name
     cursor = g.db.cursor()
@@ -571,10 +560,6 @@ def get_item_info():
     # transform datetime object to list of date elements, to be josnifiable
     item = list(cursor.fetchone())
     item[11] = datetimeToTimeElement(item[11])
-    return jsonify(t=item[3].__class__.__name__)
-
-    # decode Chinese character
-    # item[3] = urllib.unquote(item[3]).decode("utf-8")
 
     # get seller info
     userId = item[1]
