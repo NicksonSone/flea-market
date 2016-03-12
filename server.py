e8c3b6834ef5cbf9b9ba1da7dbbf411bc5d00943
sone <zhujunqi@jaxus.com>
2016-03-12 17:19:26 +0800

    auto commit

--- server.py
+++ server.py
@@ -501,7 +501,6 @@ def create_item():
     params = (userId, userName, title, categoryId, subcategoryId, price,
               arguable, tradeVenue, recency, description, delivery, postDate,
               )
-    return jsonify(insert=insert, params=params)
     cursor.execute(insert, params)
     g.db.commit()
 
